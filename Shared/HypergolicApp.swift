f811d021266ff281500371b6787ec7407b6b76cd
Don McCaughey <don@donm.cc>
2020-11-22 21:00:05 -0800

    Initial Commit

---
+++ Shared/HypergolicApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypergolicApp.swift
+//  Shared
+//
+//  Created by Don McCaughey on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct HypergolicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
