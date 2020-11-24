6783d56ea0eec02eb1171da7253f7f4d31c2b1ca
Don McCaughey <don@donm.cc>
2020-11-23 22:10:58 -0800

    In the command line tool, move the run loop.
    Only start the run loop after running the GeminiTransaction.

--- hypergolic/main.swift
+++ hypergolic/main.swift
@@ -9,9 +9,8 @@ let urlString = CommandLine.arguments.count > 1
 switch GeminiURL.parse(string: urlString) {
 case .success(let geminiURL):
     GeminiTransaction(url: geminiURL.url, delegate: Delegate()).run()
+    RunLoop.current.run()
 case .failure(let error):
     print(error.errorMessage)
     exit(EXIT_FAILURE)
 }
-
-RunLoop.current.run()
